c7e2398e3587a07a83c2f9c37c05f61df6be8e67
tnsda <tnsda@TNS_DA24>
2026-08-26 14:48:41 +0530

    product

---
+++ capgemini03/src/com/tnsif/Encapsulationprogram/Product.java
@@ -0,0 +1,29 @@
+package com.tnsif.Encapsulationprogram;
+
+class Product{
+	
+	private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		if(price>0) {
+		this.price = price;
+	}
+	else {
+		System.out.println("invalid price");
+	}
+	
+}
+
+public class Productmain {
+public static void main(String[] args) {
+	Product p=new Product();
+	p.setPrice(25000);
+	System.out.println("product price "+p.getPrice());
+	p.setPrice(-600);
+}
+}
+}
