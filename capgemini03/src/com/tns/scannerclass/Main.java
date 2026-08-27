a855f43557b8cb24123bd42c9fc3802644e9677f
Rehan Pasha <rehanpasha62p@gmail.com>
2026-08-27 19:22:27 +0530

    constructor

---
+++ capgemini03/src/com/tns/scannerclass/Main.java
@@ -0,0 +1,22 @@
+package com.tns.scannerclass;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("enter your name");
+	String name=sc.nextLine();
+	
+	System.out.println("enter your age");
+	int age=sc.nextInt();
+	
+	System.out.println("enter the salary");
+	double salary=sc.nextDouble();
+	
+	System.out.println("name-"+name);
+	System.out.println("age-"+age);
+	System.out.println("salary-"+salary);
+}
+}
