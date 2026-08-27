b138fb9e710d2aa7ae9b4090267392b9b6456bc3
Rehan Pasha <rehanpasha62p@gmail.com>
2026-08-27 19:19:31 +0530

    demo program

---
+++ capgemini03/src/com/tns/packagesprogram/Demoprogram.java
@@ -0,0 +1,12 @@
+package com.tns.packagesprogram;
+
+public class Demoprogram {
+public int a = 95;
+public String s="pasha";
+public static void main(String[] args) {
+	Demoprogram d=new Demoprogram();
+	
+	System.out.println(d.a);
+	System.out.println(d.s);
+}
+}
